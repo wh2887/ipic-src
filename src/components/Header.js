3484198fb378312b9069167e5704669c9d2f9d6b
McCallWang <13567272790@163.com>
2020-11-02 23:11:02 +0800

    Header 组件

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,86 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import {Row, Col, Button} from 'antd'
+import {Row, Col} from 'antd'
 import styled from 'styled-components'
 
 import svg from 'logo.svg'
-console.log(svg)
+
 
 const StyledRow = styled(Row)`
   background: #343A40;
+  display:flex;
+  align-items: center;
+  font-size: 22px;
+`
+const StyledImg = styled.img`
+  max-width: 64px;
+  padding: .3em;
 `
 
-const Nav = styled.nav`
-  border: 1px solid red;
+const StyledNav = styled.nav`
   text-align: center;
+  display:flex;
+  justify-content: flex-start;
+  color: white;
+  > a{
+    &:hover{color: #ccc}
+    &:active{color: #bbbfca}
+  }
+`
+const StyledNavLink = styled(NavLink)`
+  margin-left: 3em;
+  border-bottom: 1px solid transparent;
+  padding-bottom: .2em;
+  &.active{
+  border-bottom: 1px solid #bbbfca;
+  }
+`
+
+const StyledButtons = styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  flex-wrap: nowrap;
+`
+
+const StyledButton = styled.button`
+  margin: 0 .2em;
+  padding: .5em 1em;
+  font-size: 16px;
+  text-align: center;
+  color: white;
+  display:flex;
+  flex-wrap: nowrap;
+  flex-shrink: 0;
+  background: #28ABB9;
+  border-radius: 4px;
+  &:hover{
+  background: rgba(40,171,185,.8);
+  }
+  &:active{
+  background: rgba(40,171,185,.6);
+  }
 `
 
 const Header = () => {
   return (
     <StyledRow>
       <Col span={1} offset={3}>
-        <img src={svg} alt=""/>
+        <StyledImg src={svg} alt=""/>
       </Col>
       <Col span={14}>
-        <Nav>
-          <NavLink to='/' exact>首页</NavLink>
-          <NavLink to='/history'>上传历史</NavLink>
-          <NavLink to='/about'>关于我</NavLink>
-        </Nav>
+        <StyledNav>
+          <StyledNavLink to='/' exact activeClassName="active">首页</StyledNavLink>
+          <StyledNavLink to='/history'>上传历史</StyledNavLink>
+          <StyledNavLink to='/about'>关于我</StyledNavLink>
+        </StyledNav>
       </Col>
       <Col span={3}>
-        <Button  style={{ color:"white",background: "#28ABB9", borderColor: "#ccc" }}>登录</Button>
-        <Button type="primary">注册</Button>
+        <StyledButtons>
+          <StyledButton>登录</StyledButton>
+          <StyledButton>注册</StyledButton>
+        </StyledButtons>
       </Col>
       <Col span={3}>
 
